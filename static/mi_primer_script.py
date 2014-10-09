d715dc0fe4512a386cafd20a1fb4a420c73b0482
Álex Sáez <alex.saez.12@gmail.com>
2014-10-09 21:13:55 +0200

    creada carpeta static para contener imagenes, datos, scripts...

---
+++ static/mi_primer_script.py
@@ -0,0 +1,11 @@
+import math
+     
+print("Hola gente del curso de AeroPython")
+print("¿Cuántos sois hoy en clase?")
+
+number = input()
+number = int(number)
+root = math.sqrt(number)
+
+print("Ufff! eso es un montón! espero que aprendáis mucho")
+print("Por cierto, la raiz de %i es %f" %(number, root))
